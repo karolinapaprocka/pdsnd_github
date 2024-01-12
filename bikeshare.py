55b18e39811c0af8e387b4d72907259ba117c757
karolinapaprocka <karolina.paprocka@bshg.com>
2024-01-12 16:30:24 +0100

    add title comment and print welcome message

--- bikeshare.py
+++ bikeshare.py
@@ -1,3 +1,5 @@
+#Bikeshare project 
+
 import time
 import pandas as pd
 import numpy as np
@@ -19,8 +21,11 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+print('Hello!')
+print ()
+
     # Get user input for city (chicago, new york city, washington).
-    print(' Hello! Let\'s explore some US bikeshare data!\n','Which city would you like to see the data for: Chicago, New York City, or Washington?\n')
+    print(' Let\'s explore some US bikeshare data!\n','Which city would you like to see the data for: Chicago, New York City, or Washington?\n')
     city = ''
     while city not in city_data:
         city = input('Enter your city: ').lower()
